c24fb1fc88e5514ea61e23fed5dfac182b8f1af8
tibssy <norbert.tiborcz@gmail.com>
2023-01-19 06:42:37 +0000

    test Workflow

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -82,6 +82,7 @@ def get_dealerships(request):
 # def get_dealer_details(request, dealer_id):
 # ...
 
+
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
