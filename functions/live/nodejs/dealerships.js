2912d5501083af95e2f1bc51be9114440af49003
tibssy <norbert.tiborcz@gmail.com>
2023-01-21 05:42:26 +0000

    Web Actions for ibmcloudant

---
+++ functions/live/nodejs/dealerships.js
@@ -0,0 +1,17 @@
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+
+async function main(params) {
+    const dbName = "dealerships"
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({
+        authenticator: authenticator
+        });
+    cloudant.setServiceUrl(params.COUCH_URL);
+    try {
+        let dealerships = await cloudant.postFind({ db: dbName, selector:{state:params.state}});
+        return { body: dealerships.result.docs}
+    } catch (error) {
+        return { error: error.description };
+    }
+}
---
+++ functions/live/python/reviews.py
@@ -0,0 +1,25 @@
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_cloud_sdk_core import ApiException
+
+
+def main(param_dict):
+    db_name = "reviews"
+    keyword = "dealerId"
+    try:
+        authenticator = IAMAuthenticator(param_dict["IAM_API_KEY"])
+        service = CloudantV1(authenticator=authenticator)
+        service.set_service_url(param_dict["COUCH_URL"])
+
+        if keyword in param_dict:
+            selector = {"dealership": int(param_dict[keyword])}
+            filtered = service.post_find(db=db_name, selector=selector)
+            return { "body": filtered.result['docs']}
+
+        reviews = service.post_all_docs(db=db_name, include_docs=True)
+        return {"body": [i['doc'] for i in reviews.result['rows']]}
+
+    except ApiException as ae:
+        if ("reason" in ae.http_response.json()):
+            return {"error": ae.http_response.json()["reason"]}
+        return {"status_code": ae.code, "error_message": ae.message}
